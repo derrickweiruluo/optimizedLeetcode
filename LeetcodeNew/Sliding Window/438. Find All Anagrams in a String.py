6e176780a9b397cb79d51661e14e6dcbc26a28d7
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-08-01 21:10:56 -0400

    Counter(window) as a sliding window

--- LeetcodeNew/Sliding Window/438. Find All Anagrams in a String.py
+++ LeetcodeNew/Sliding Window/438. Find All Anagrams in a String.py
@@ -1,3 +1,22 @@
+"""
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+Example 1:
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+
+Example 2:
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+"""
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
