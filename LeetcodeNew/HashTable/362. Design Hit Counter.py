0950f4794161e892281ce24d97e9396d11783745
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-08-18 21:30:06 -0400

    Create 362. Design Hit Counter.py

---
+++ LeetcodeNew/HashTable/362. Design Hit Counter.py
@@ -0,0 +1,57 @@
+"""
+Design a hit counter which counts the number of hits received in the past 5 minutes (i.e., the past 300 seconds).
+
+Your system should accept a timestamp parameter (in seconds granularity), and you may assume that calls are being made to the system in chronological order (i.e., timestamp is monotonically increasing). Several hits may arrive roughly at the same time.
+
+Implement the HitCounter class:
+
+HitCounter() Initializes the object of the hit counter system.
+void hit(int timestamp) Records a hit that happened at timestamp (in seconds). Several hits may happen at the same timestamp.
+int getHits(int timestamp) Returns the number of hits in the past 5 minutes from timestamp (i.e., the past 300 seconds).
+
+################
+
+Constraints:
+
+1 <= timestamp <= 2 * 109
+All the calls are being made to the system in chronological order (i.e., timestamp is monotonically increasing).
+At most 300 calls will be made to hit and getHits.
+"""
+
+
+class HitCounter:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.counter = collections.Counter()
+        
+
+    def hit(self, timestamp: int) -> None:
+        """
+        Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity).
+        """
+        self.counter[timestamp] += 1
+
+    def getHits(self, timestamp: int) -> int:
+        """
+        Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity).
+        """
+        res = 0
+        # ex: for time btw 1 to 300 
+        # O(n) for the below accumulating
+        # for i in range(timestamp - 299, timestamp + 1):
+        #     res += self.counter[i]
+        for t in self.counter:
+            if t in range(timestamp - 299, timestamp + 1):
+                res += self.counter[t]
+        return res
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
