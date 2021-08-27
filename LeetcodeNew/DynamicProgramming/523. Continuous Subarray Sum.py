3345f8b6cf4e600256f2ee3ae4a03588af20b6d6
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-08-27 00:43:45 -0400

    prefix sum of remainder

--- LeetcodeNew/DynamicProgramming/523. Continuous Subarray Sum.py
+++ LeetcodeNew/DynamicProgramming/523. Continuous Subarray Sum.py
@@ -1,20 +1,53 @@
 # prefix sum
+"""
+Given an integer array nums and an integer k, return true if nums has a continuous subarray of size at least two whose elements sum up to a multiple of k, or false otherwise.
+
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+Example 1:
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+
+Example 2:
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+
+Example 3:
+Input: nums = [23,2,6,4,7], k = 13
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+0 <= sum(nums[i]) <= 231 - 1
+1 <= k <= 231 - 1
+
+##########
+prefix sum, and calculaute the prefix sum remainder of k
+if this remainder happended before, and len >= 2: return True
+"""
+
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         
-        seen = {0: -1}
+        seen = {0: -1}  # 最重要的初始化，保证长度 >= 2
         curr_remainder = 0
         
         for idx, val in enumerate(nums):
-            curr_remainder = (curr_remainder + val) % k   # MOST IMPORTANT
+            curr_remainder = (curr_remainder + val) % k   # prefix_sum of remainder
             if curr_remainder not in seen:
                 seen[curr_remainder] = idx
             else:
                 # this a step to make sure valid range: len > 2
                 # corner cases:
-                # if only the first one iteself satisfy, len == 1: False
-                # if the first two together satisfy:     len = 2 - 0 > 1: True
-                if idx - seen[curr_remainder] > 1:      # remainder check is 左闭右开，所以要 > 1才符合
+                # if only the first one iteself satisfy, 
+                # if the first two together satisfy:     
+                if idx - seen[curr_remainder] > 1:      
                     print(idx, seen[curr_remainder])
                     return True
         
