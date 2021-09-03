3eb175d768bddf1a6bd78d5761cb54c7fe8d7a6b
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-09-02 23:20:42 -0400

    Update 340. Longest Substring with At Most K Distinct Characters.py

--- LeetcodeNew/Sliding Window/340. Longest Substring with At Most K Distinct Characters.py
+++ LeetcodeNew/Sliding Window/340. Longest Substring with At Most K Distinct Characters.py
@@ -1,18 +1,38 @@
+"""
+Given a string s and an integer k, return the length of the longest substring of s that contains at most k distinct characters.
+
+ 
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: The substring is "ece" with length 3.
+
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: The substring is "aa" with length 2.
+ 
+Constraints:
+
+1 <= s.length <= 5 * 104
+0 <= k <= 50
+
+"""
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
         
+        left = res = 0
         counter = collections.Counter()
-        res, left, distinct_count = 0, 0, 0
         
-        for idx, char in enumerate(s):
-            if counter[char] == 0:
-                distinct_count += 1
+        for i, char in enumerate(s):
             counter[char] += 1
-            while distinct_count > k:
+            while len(counter) > k:
                 counter[s[left]] -= 1
                 if counter[s[left]] == 0:
-                    distinct_count -=1
+                    del counter[s[left]]
                 left += 1
-            res = max(res, idx - left + 1)
-            
+            res = max(res, i - left + 1)
+        
         return res
