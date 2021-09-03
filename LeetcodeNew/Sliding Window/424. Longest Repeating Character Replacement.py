af9ff3355bbd5cbdb9f5c8f506ca9ba163e38cd9
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-09-03 00:12:08 -0400

    Update 424. Longest Repeating Character Replacement.py

--- LeetcodeNew/Sliding Window/424. Longest Repeating Character Replacement.py
+++ LeetcodeNew/Sliding Window/424. Longest Repeating Character Replacement.py
@@ -1,17 +1,48 @@
+"""
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length
+
+
+
+#####
+counter 滑窗基操
+"""
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         
-        max_char_count, res = 0, 0
         counter = collections.Counter()
+        max_freq = res = left = 0
         
-        for idx, char in enumerate(s):
+        for i, char in enumerate(s):
             counter[char] += 1
-            max_char_count = max(max_char_count, counter[char])
+            max_freq = max(max_freq, counter[char])
             
-            if res < max_char_count + k:
+            if max_freq + k > res:
                 res += 1
             else:
-                # equivalent to move the left pointer by descreasing the leftmost counter by 1
-                counter[s[idx - res]] -= 1
+                counter[s[left]] -= 1
+                left += 1
         
         return res
