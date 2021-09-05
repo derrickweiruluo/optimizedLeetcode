bd8c388625303be77260e01f0599df2c7d2382ed
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-09-04 20:38:51 -0400

    Update 76. Minimum Window Substring.py

--- LeetcodeNew/Sliding Window/76. Minimum Window Substring.py
+++ LeetcodeNew/Sliding Window/76. Minimum Window Substring.py
@@ -26,27 +26,25 @@ Since the largest window of s only has one 'a', return empty string.
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        res = len(s) + 1
-        missing = len(t)
-        
         target = collections.Counter(list(t))
-        start, end = 0, 0
-        left = 0
+        missing = len(t)
+        start = end = 0
+        i = 0
         
-        for right, char in enumerate(s, 1):  # right start from 1
+        for j, char in enumerate(s):
             if target[char] > 0:
                 missing -= 1
             target[char] -= 1
             if missing == 0:
-                while left < right and target[s[left]] < 0:
-                    target[s[left]] += 1
-                    left += 1
-                target[s[left]] += 1
-                missing += 1
-                if end == 0 or right - left < end - start:
-                    start, end = left, right
+                while i < j and target[s[i]] < 0:
+                    target[s[i]] += 1
+                    i += 1
+                if end == 0 or j - i < end - start:
+                    start, end = i, j + 1
                 
-                left += 1
+                target[s[i]] += 1
+                i += 1
+                missing += 1
         
         return s[start: end]
         
