fd0d5834049da6467c1a16208f1f299c1777d57d
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-09-05 20:33:38 -0400

    Create 1676. Lowest Common Ancestor of a Binary Tree IV.py

---
+++ LeetcodeNew/Tree/1676. Lowest Common Ancestor of a Binary Tree IV.py
@@ -0,0 +1,56 @@
+"""
+Given the root of a binary tree and an array of TreeNode objects nodes, return the lowest common ancestor (LCA) of all the nodes in nodes. All the nodes will exist in the tree, and all values of the tree's nodes are unique.
+
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [1]
+Output: 1
+Explanation: The lowest common ancestor of a single node is the node itself.
+
+Example 3:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [7,6,2,4]
+Output: 5
+Explanation: The lowest common ancestor of the nodes 7, 6, 2, and 4 is node 5.
+
+Example 4:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [0,1,2,3,4,5,6,7,8]
+Output: 3
+Explanation: The lowest common ancestor of all the nodes is the root node.
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-109 <= Node.val <= 109
+All Node.val are unique.
+All nodes[i] will exist in the tree.
+All nodes[i] are distinct.
+
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        node_set = set(nodes)
+        
+        def traverse(root, node_set):
+            if not root: return
+            if root in node_set:
+                return root
+            
+            left = traverse(root.left, node_set)
+            right = traverse(root.right, node_set)
+            
+            if left and right:
+                return root
+            if left: return left
+            if right: return right
+            else: return None
+        
+        return traverse(root, node_set)
