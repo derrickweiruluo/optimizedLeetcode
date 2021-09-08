a7371a5d09f0a977c252cae8a9346464a69966a5
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-09-07 23:28:34 -0400

    Update 34. Find First and Last Position of Element in Sorted Array.py

--- LeetcodeNew/BinarySearch/34. Find First and Last Position of Element in Sorted Array.py
+++ LeetcodeNew/BinarySearch/34. Find First and Last Position of Element in Sorted Array.py
@@ -23,6 +23,9 @@ Constraints:
 nums is a non-decreasing array.
 -109 <= target <= 109
 
+#####
+def一个通用二分函数， 找target的左位置， 找target + 1 的前一位
+确认边界和确认target存在
 """
 
 
@@ -32,25 +35,21 @@ class Solution:
         if not nums: return [-1, -1]
         
         n = len(nums)
-        if n == 1: 
-            if nums[0] == target:
-                return [0, 0]
-            else:
-                return [-1, -1]
+        if n == 1: return [0, 0] if nums[0] == target else [-1, -1]
         
         def search(target):
             left, right = 0, n
             while left < right:
-                mid = left + (right - left) // 2
-                if nums[mid] >= target:
-                    right = mid
-                else:
+                mid = (left + right) // 2
+                if nums[mid] < target:
                     left = mid + 1
+                else:
+                    right = mid
             return left
         
         lo = search(target)
-        if lo > n - 1:return [-1, -1]
-        if target == nums[lo]:
-            return [lo, search(target + 1) - 1]
+        if lo >= n or nums[lo] != target:
+            return [-1, -1]
         
-        return [-1, -1]
+        hi = search(target + 1) - 1
+        return [lo, hi]
