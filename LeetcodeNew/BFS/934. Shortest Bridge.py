227a408b2500d53a7caa8221b668db14bae18090
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-09-12 22:47:38 -0400

    Update 934. Shortest Bridge.py

--- LeetcodeNew/BFS/934. Shortest Bridge.py
+++ LeetcodeNew/BFS/934. Shortest Bridge.py
@@ -1,3 +1,39 @@
+"""
+In a given 2D binary array grid, there are two islands.  (An island is a 4-directionally connected group of 1s not connected to any other 1s.)
+
+Now, we may change 0s to 1s so as to connect the two islands together to form 1 island.
+Return the smallest number of 0s that must be flipped.  (It is guaranteed that the answer is at least 1.)
+
+ 
+
+Example 1:
+
+Input: grid = [[0,1],[1,0]]
+Output: 1
+Example 2:
+
+Input: grid = [[0,1,0],[0,0,0],[0,0,1]]
+Output: 2
+Example 3:
+
+Input: grid = [[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+Output: 1
+ 
+
+Constraints:
+2 <= grid.length == grid[0].length <= 100
+grid[i][j] == 0 or grid[i][j] == 1
+
+#####思路
+1. 先随机找到第一个点
+2. 然后围绕着这个点，在grid内，把这个岛周边的海洋boundary，记录到一个hashset
+2.1 dfs下一个相邻陆地，直到这个岛被search完，which意味着boundary也记录完
+3. 把第一个岛屿的boundary来个BFS， return steps
+
+注意： 以上的DFS， BFS步骤，每找到一块陆地都以第三方标记记录
+
+
+"""
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         
