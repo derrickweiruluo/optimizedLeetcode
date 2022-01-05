6e6e808b6dcd0ae543e2fd04c8ecf037a396848e
derrickweiruluo <derrickweiruluo@gmail.com>
2022-01-05 18:07:17 -0500

    01052022B

--- Companies/FB/140 Word Break II.py
+++ Companies/FB/140 Word Break II.py
@@ -1,12 +1,6 @@
 '''
 Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
 
-Note that the same word in the dictionary may be reused multiple times in the segmentation.
-
- 
-
-Example 1:
-
 Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
 Output: ["cats and dog","cat sand dog"]
 Example 2:
@@ -14,7 +8,6 @@ Example 2:
 Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
 Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
 Explanation: Note that you are allowed to reuse a dictionary word.
-
 '''
 
 class Solution:  # 20ms  interview
